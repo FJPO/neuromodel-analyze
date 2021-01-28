ab88c24ac0e0fcc9e2877b6aa23abd54d7b407ea
etha-prime <68636291+etha-prime@users.noreply.github.com>
2021-01-28 11:23:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+def model_analyze(log):
+  # model = keras.model.load_model(path)
+  # Выводим график точности на обучающей выборке
+  # label - имя графика в легенде
+  plt.plot(log.history['accuracy'], 
+          label='Доля верных ответов на обучающем наборе')
+  print(log.history.keys())
+  # Выводим график точности на проверочной выборке
+  plt.plot(log.history['val_accuracy'], 
+          label='Доля верных ответов на проверочном наборе')
+
+  # Выводим подписи осей
+  plt.title("Точность")
+  plt.xlabel('Эпоха обучения')
+  plt.ylabel('Доля верных ответов')
+
+  # Выводим легенду
+  plt.legend()
+  plt.show()
+
+  plt.plot(log.history['loss'], label='Ошибка на обучающем наборе')
+  plt.plot(log.history['val_loss'], label='Ошибка на проверочном наборе')
+  plt.title("Ошибка")
+  plt.xlabel('Эпоха обучения')
+  plt.ylabel('Ошибка')
+  plt.legend()
+  plt.show()
